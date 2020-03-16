cf5caa7298639d97fc57de2013989f7545e0b564
Andrew Moryakov <topazd2@gmail.com>
2020-03-16 19:37:47 +0300

    В модель задания печати добавлен объект описывающий задание и методы для управления состоянием задания

--- Project53/New arhtech/JobMeta.cs
+++ Project53/New arhtech/JobMeta.cs
@@ -1,16 +1,45 @@
+using System;
+using System.Printing;
+
 namespace Project53.New_arhtech
 {
     public class JobMeta
     {
+        public PrintSystemJobInfo _job; 
         public string DocumentName { get; private set; }
         public int CountOfPages { get; private set; }
         public int Copies { get; private set; }
-        
-        public JobMeta(string documentName, int countOfPages, int copies)
+
+        public void CheckJob()
         {
+            if(_job == null)
+                throw new ArgumentNullException("Задание на печать не инициализировано.");
+        }
+
+        public void CancelJob()
+        {
+            CheckJob();
+            _job.Cancel();
+        }
+
+        public void Resume()
+        {
+            CheckJob();
+            _job.Resume();
+        }
+
+        public JobMeta(string documentName, int countOfPages, int copies, PrintSystemJobInfo job)
+        {
+            _job = job;
             DocumentName = documentName;
             CountOfPages = countOfPages;
             Copies = copies;
+        }
+
+        public void ToPause()
+        {
+            CheckJob();
+            _job.Pause();
         }
     }
 }
