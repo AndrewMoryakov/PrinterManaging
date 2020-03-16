dbdacf86b71b0996c8a64ca96d72e701acd10c13
Andrew Moryakov <topazd2@gmail.com>
2020-03-16 19:38:56 +0300

    Сформулирована структура выполнения бизнес логики.

--- Project53/Program.cs
+++ Project53/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Project53.New_arhtech;
 using Serilog;
+using Serilog.Core;
 using Serilog.Sinks.SystemConsole.Themes;
 
 namespace Project53
@@ -9,23 +11,52 @@ namespace Project53
 //https://stackoverflow.com/questions/6103705/how-can-i-send-a-file-document-to-the-printer-and-have-it-print
     internal class Program
     {
-        public static List<string> PrintingMsDocs = new List<string>(); 
-        public static void Main(string[] args)
-        {   
-            var logger = new LoggerConfiguration()
+        private static Logger _logger;
+        public static List<string> PrintingMsDocs;
+        private static BaseValidationOfDoc _validator;  
+
+        private static void Configure()
+        {
+            _validator = new BaseValidationOfDoc();
+            PrintingMsDocs = new List<string>();
+            _logger = new LoggerConfiguration()
                 .WriteTo.Console(theme: SystemConsoleTheme.Colored)
                 .WriteTo.File("logs.log")
-                .MinimumLevel.Debug()
+                .MinimumLevel.Verbose()
                 .CreateLogger();
-            
-            var supprtedPrinters = Printers.Get();
+        }
 
-            foreach (var printerName in supprtedPrinters)
+        public static void Main(string[] args)
+        {
+            Configure();
+            ProcessWithPrinters();
+            Console.ReadKey();
+        }
+
+        private static void ProcessWithPrinters()
+        {
+            List<string> GetPrinters()
             {
-                var printer = new PrinterWrapper(printerName);
-                printer.SubscribeOnPrintEvent(Subscriber);
+                var list = Printers.Get().ToList();
+                _logger.Information("Printers");
+                list.ForEach(el => _logger.Verbose(el));
+                return list;
+            }
+            void SubscribePrinters(List<string> supprtedPrinters1)
+            {
+                foreach (var printerName in supprtedPrinters1)
+                {
+                    var printer = new PrinterWrapper(printerName);
+                    printer.SubscribeOnPrintEvent(Subscriber);
+                }
             }
             
+            var supprtedPrinters = GetPrinters();
+            SubscribePrinters(supprtedPrinters);
+        }
+
+        private void StartOldFunctions()
+        {
             //var printers;
             
             //RunSpoolerService
@@ -34,12 +65,22 @@ namespace Project53
             // var supprtedPrinters = Printers.Get();
             // JobController jobController = new JobController(supprtedPrinters, logger);
             // jobController.StartWatching();
-
-            Console.ReadKey();
         }
 
-        private static void Subscriber(JobMeta obj)
+        private static void Subscriber(JobMeta job)
         {
+            job.ToPause();
+            var jobIsValid = _validator.CheckJob(job);
+
+            if (jobIsValid == false)
+                job.CancelJob();
+
+            Client client = Auth.GetClient();
+
+            var canToPrint = ClientValidator.CanContinuePrinting(client, job);
+
+            if (canToPrint == true)
+                job.Resume();
         }
     }
 }
