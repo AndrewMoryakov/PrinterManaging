f314a3a478ce1de80d82a5a63ee3f229fde8a2cf
Andrew Moryakov <topazd2@gmail.com>
2020-03-16 19:33:47 +0300

    Модель описывающая клиента.

---
+++ Project53/New arhtech/Client.cs
@@ -0,0 +1,12 @@
+namespace Project53.New_arhtech
+{
+	public class Client
+	{
+		public decimal Balance { get; private set; }
+
+		public Client(decimal balance)
+		{
+			Balance = balance;
+		}
+	}
+}
