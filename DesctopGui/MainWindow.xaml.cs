c9c206f1fb4a17247a159a728279787ef6872c2c
Andrew Moryakov <topazd2@gmail.com>
2020-04-19 20:15:01 +0300

    Wpf start

---
+++ DesctopGui/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace DesctopGui
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void TextBoxPassword_OnKeyDown(object sender, KeyEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ButtonAuth_OnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ButtonVk_OnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void ButtonSignUp_OnClick(object sender, RoutedEventArgs e)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
