a62e4a548132a4c7377e93930de5d3708cc878c1
Andrew Moryakov <topazd2@gmail.com>
2020-05-04 14:41:19 +0300

    UserController рефакторинг

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -1,21 +1,33 @@
+#nullable disable
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Controllers.Helpers;
 using WebApplication1.DataModel;
 
 namespace WebApplication1.Controllers
 {
 	[Produces("application/json")]
 	[Route("api/[controller]")]
-	// [Authorize]
+	[Authorize]
 	[ApiController]
-	public class User : ControllerBase
+	public class UserController : ControllerBase
 	{
+			
+		private readonly IUserService _userService;
+
+		public UserController(IUserService userService) =>
+			(_userService) = (userService);
+
 		[HttpGet]
 		public async Task<ActionResult<UserInfoVm>> Get()
 		{
 			return new UserInfoVm("username@gmail.com", "Andrew", "Moryakov", 10m);
 
+			
+			
+			
 			return BadRequest();
 		}
 	}
