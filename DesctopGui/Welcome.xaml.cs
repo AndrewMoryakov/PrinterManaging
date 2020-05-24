70ce77223e40608c2020f5eeab78d6146af91c75
topazd2@gmail.com <github72453722>
2020-05-24 20:49:29 +0300

    Публикую токен, чтобы использовать в других местах в приложении. В странице пользователя в GUI работает логаут и используется токен для обновления информации и для логаута

--- DesctopGui/Welcome.xaml.cs
+++ DesctopGui/Welcome.xaml.cs
@@ -1,5 +1,8 @@
+using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using BackendClient;
 using DesctopGui.DateModels;
 using DreamPlace.Lib.Rx;
 
@@ -7,22 +10,35 @@ namespace DesctopGui
 {
 	public partial class Welcome : Page
 	{
+		private PrintGoClient _clientToPrint;
+		private ClientToBack _clientToBackOfServers;
 		public Welcome()
 		{
 			InitializeComponent();
 
 			var user = Registry<UserInfo, UserInfo>.Get();
 			textBlockBalance.Text = $"{user.Balance.ToString()} ₽";
+			_clientToPrint = Registry.GetValue<PrintGoClient>();
+			_clientToBackOfServers = Registry.GetValue<ClientToBack>();
 		}
 
-		private void ButtonLogOut_OnClick(object sender, RoutedEventArgs e)
+		private async void ButtonLogOut_OnClick(object sender, RoutedEventArgs e)
 		{
-			throw new System.NotImplementedException();
+			//ClearIECookies.Clear("https://oauth.vk.com");
+			await _clientToPrint.LogOutAsync(Registry.GetValue<string>("token"));
+			//Environment.Exit(0);
 		}
 
 		private void ButtonBalanseRefresh_OnClick(object sender, RoutedEventArgs e)
 		{
-			throw new System.NotImplementedException();
+			//ToDo
+			//надо оповещать P53 о том,
+			//что сделали рефреш баланса, чтобы P53 тоже обновил себя
+			
+			textBlockBalance.Text =
+				_clientToBackOfServers
+					.GetUserInfo(Registry.GetValue<string>("token")).Balance
+					.ToString(CultureInfo.InvariantCulture);
 		}
 	}
 }
