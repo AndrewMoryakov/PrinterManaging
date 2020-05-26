ee0625640615b57f8b56006799d9a354e626e55a
topazd2@gmail.com <github72453722>
2020-05-26 10:42:32 +0300

    К данным добавлен пустой конструктор

--- DataModels/PrintedDocument.cs
+++ DataModels/PrintedDocument.cs
@@ -4,6 +4,10 @@ namespace DataModels
 {
     public class PrintedDocument
     {
+        public PrintedDocument()
+        {
+        }
+        
         public PrintedDocument(string docName, int amountOfPages)
         {
             Document = docName;
